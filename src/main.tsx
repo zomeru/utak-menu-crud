d2215a1ac5a6ba866d8c2836a772c237a8f48c6b
zomeru <zomerzxc.19@gmail.com>
2024-03-08 22:39:39 +0800

    Final tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -10,6 +10,10 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+  },
+  {
+    path: '/*',
+    element: <Navigate to='/' />,
   },
 ]);
 
